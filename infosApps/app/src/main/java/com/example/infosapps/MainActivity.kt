6fea58236c784e31c7cdf651336770ae92ff2e39
agonglomarel7 <agonglomarel@gmail.com>
2024-10-31 19:45:03 +0100

    Kotlin Infos App

--- infosApps/app/src/main/java/com/example/infosapps/MainActivity.kt
+++ infosApps/app/src/main/java/com/example/infosapps/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.infosapps
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,21 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
+
+    fun tryToConnect(view: View){
+        val email = findViewById<EditText>(R.id.editTextTextEmailAddress)
+        val password = findViewById<EditText>(R.id.editTextTextPassword)
+
+        val emailValue = email.text.toString()
+        val passwordValue = password.text.toString()
+
+        AlertDialog
+            .Builder(this)
+            .setTitle("Demande de Connexion")
+            .setMessage("Vos information de connexion sont : \n Email : $emailValue \n PassWord : $passwordValue ")
+            .show()
+    }
+
 }
